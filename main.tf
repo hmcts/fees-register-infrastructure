72ce1d9bf8fad0ae53da0aa5bb71ec2728942311
Tim Jacomb <t.jacomb@kainos.com>
2017-11-27 18:20:55 +0000

    Remove credentials (#1)

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "fees-register-api" {
-  source   = "git::https://23a108ab5ea17c28372a130d72aa60ea0761839b@github.com/contino/moj-module-webapp?ref=0.0.78"
+  source   = "git@github.com:contino/moj-module-webapp?ref=0.0.78"
   product  = "${var.product}-api"
   location = "${var.location}"
   env      = "${var.env}"
@@ -14,7 +14,7 @@ module "fees-register-api" {
 }
 
 module "fees-register-database" {
-  source              = "git::https://23a108ab5ea17c28372a130d72aa60ea0761839b@github.com/contino/moj-module-postgres?ref=master"
+  source              = "git@github.com:contino/moj-module-postgres?ref=master"
   product             = "${var.product}"
   location            = "West Europe"
   env                 = "${var.env}"
