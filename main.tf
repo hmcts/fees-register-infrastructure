d8a0b5688350d093a1010d66c0e3a553b6ad247d
jaleen <jalal.din@gmail.com>
2017-12-20 15:59:09 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ module "fees-register-api" {
     POSTGRES_USER     = "${module.fees-register-database.user_name}"
     SPRING_DATASOURCE_USERNAME = "${module.fees-register-database.user_name}"
     SPRING_DATASOURCE_URL = "jdbc:postgresql://${module.fees-register-database.host_name}:${module.fees-register-database.postgresql_listen_port}/${module.fees-register-database.postgresql_database}"
-    SPRING_DATASOURCE_PASSWORD = "${module.fees-register-database.password}"
+    SPRING_DATASOURCE_PASSWORD = "${var.database-password}"
   }
 }
 
